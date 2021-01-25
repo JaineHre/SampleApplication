83c2d68c5193d0637b28847d4667708769d4ad8c
JaineHre <JaineHre@users.noreply.github.com>
2021-01-25 18:07:02 +0200

    Initial commit

---
+++ SampleApplication/Controllers/UsersController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SampleApplication.Database;
+using SampleApplication.Entities;
+
+namespace SampleApplication.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly DatabaseContext context;
+
+        public UsersController(DatabaseContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+        {
+            return await context.Users.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<AppUser>> GetUserById(int id)
+        {
+            return await context.Users.FindAsync(id);
+        }
+    }
+}
---
+++ SampleApplication/Database/DatabaseContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SampleApplication.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SampleApplication.Database
+{
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext(DbContextOptions options) : base(options)
+        { }
+
+        public DbSet<AppUser> Users { get; set; }
+    }
+}
